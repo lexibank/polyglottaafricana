4726d2a339ca10cf8632a1ca69a36e07618670bd
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-04-21 19:49:03 +0200

    Add tests

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
-
 def test_valid(cldf_dataset, cldf_logger):
     assert cldf_dataset.validate(log=cldf_logger)
+
+def test_forms(cldf_dataset):
+    assert len(list(cldf_dataset["FormTable"])) == 54073
+    assert any(f["Form"] == "kidā́re" for f in cldf_dataset["FormTable"])
+    assert any(f["Form"] == "yáhāre" for f in cldf_dataset["FormTable"])
+
+
+def test_languages(cldf_dataset, cldf_logger):
+    assert len(list(cldf_dataset["LanguageTable"])) == 159
+
+
+def test_parameters(cldf_dataset, cldf_logger):
+    assert len(list(cldf_dataset["ParameterTable"])) == 335
+
+
+def test_sources(cldf_dataset, cldf_logger):
+    assert len(cldf_dataset.sources) == 1
